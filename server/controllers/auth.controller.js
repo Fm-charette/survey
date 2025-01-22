ffd0ce50cfabe15d4386608a1ad8c454f07be524
Floryan <mollet.floryan@gmail.com>
2025-01-22 22:21:19 +0100

    Fix : Add try catch in the auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,13 @@
-const { userService, tokenService } = require('../services/')
+const { userService, tokenService } = require('../services/');
 
 const register = async (req, res) => {
-  const user = await userService.createUser(req.body);
-  const tokens = await tokenService.generateAuthTokens(user);
-  res.status(httpStatus.CREATED).send({ user, tokens });
+  try {
+    const user = await userService.createUser(req.body);
+    const tokens = await tokenService.generateAuthTokens(user);
+    res.status(httpStatus.CREATED).send({ user, tokens });
+  } catch (err) {
+    res.status(500).send({ error: error.message });
+  }
 }
 
 module.exports = {
