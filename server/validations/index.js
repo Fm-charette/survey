bfa90ef4180d83df8a45f6d8f51b56b87f9c0be7
Floryan <mollet.floryan@gmail.com>
2025-01-25 16:48:06 +0100

    Fix: Add user validation in the index file for validation

--- server/validations/index.js
+++ server/validations/index.js
@@ -1,2 +1,3 @@
 module.exports.listValidation = require('./list.validation');
 module.exports.authValidation = require('./auth.validation');
+module.exports.userValidation = require('./user.validation');
