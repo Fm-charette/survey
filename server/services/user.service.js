2403d71521b5c1935683e00662bb0b41cecd5f43
Floryan <mollet.floryan@gmail.com>
2025-01-26 17:52:52 +0100

    Fix: Add utils files for handeling api error

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,11 +1,10 @@
 const { User } = require('../models');
+const apiError = require('../utils/api.error')
 
 const createUser = async (userBody) => {
-  console.log(userBody);
   const isEmailTaken = await User.findOne({ email: userBody.email });
-  console.log(isEmailTaken);
  if (isEmailTaken)
-    throw new ApiError('BAD REQUEST', 'Email already taken');
+    throw apiError(400, 'Email already taken');
   return User.create(userBody);
 };
 
@@ -16,26 +15,26 @@ const queryUser = async() => {
 const findById = async(id) => {
   const user = await User.findById(id);
   if (!user)
-    throw new ApiError('NOT FOUND', 'User not found');
+    throw createError(404, 'User not found')
   return user;
 }
 
 const updateById = async(id, body) => {
   if (Object.keys(body).length === 0)
-    throw new ApiError('BAD REQUEST', 'No fields provided for update');
+    throw createError(400, 'No fields provided for update');
   if (body.email && (await User.isEmailTaken(body.email, id)))
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Email already taken');
+    throw createError(400, 'Email already taken');
   const updatedUser = await User.findByIdAndUpdate(id, body, { new: true });
   if (!updatedUser)
-    throw new ApiError('NOT FOUND', 'User not found');
+    throw createError(404, 'User not found');
   return updatedUser;
 }
 
 const deleteOne = async(id) => {
   try {
     return await User.deleteOne({ _id: id });
-  } catch (err) {
-    throw new Error("Erreur lors de la suppression du document", err);
+  } catch {
+    throw createError(404, 'User not found');
   }
 }
 module.exports = {
