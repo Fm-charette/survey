a66a0be57275a6d53d9db22d09b197caaa1653c4
Floryan <mollet.floryan@gmail.com>
2025-02-15 13:17:06 +0100

    Fix: Update list service, add apiError for error handling

--- server/services/list.service.js
+++ server/services/list.service.js
@@ -1,24 +1,42 @@
-const { ListItem } = require('../models');
+const { List } = require('../models');
+const apiError = require('../utils/api.error');
 
 const createList = async (body) => {
-  return ListItem.create(body);
+  try {
+    return List.create(body);
+  } catch (error) {
+    throw apiError(400, error);
+  }
 }
 
 const query = async () => {
-  return ListItem.find();
+  try {
+    return List.find();
+  } catch {
+    throw apiError(404, 'Cannot find list');
+  }
 };
 
 const findById = async (id) => {
-  return ListItem.findById(id);
+  try {
+    const list = await List.findById(id);
+    if (!list) {
+      throw apiError(404, 'List not found');
+    }
+    return List.findById(id);
+  } catch (error) {
+    throw apiError(400, error)
+  }
+
 };
 
 const updateById = async (id, body) => {
-  return ListItem.findByIdAndUpdate(id, body);
+  return List.findByIdAndUpdate(id, body);
 }
 
 const deleteOne = async (id) => {
   try {
-    return await ListItem.deleteOne({ _id: id });
+    return await List.deleteOne({ _id: id });
   } catch (error) {
     console.log(error);
     throw new Error("Erreur lors de la suppression du document");
